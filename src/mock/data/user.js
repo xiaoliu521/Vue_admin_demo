4f78c2535267483f8ca5095e4e65b2a31c369043
username <xianrenzy1024@163.com>
2019-12-04 16:15:25 +0800

    样例项目代码

---
+++ src/mock/data/user.js
@@ -0,0 +1,28 @@
+import Mock from 'mockjs';
+// mockjs作用
+// 1.前后端分离 2.不需要修改既有代码，就可以拦截 Ajax 请求，返回模拟的响应数据。
+// 3.数据类型丰富  4.通过随机数据，模拟各种场景。
+const LoginUsers = [
+  {
+    id: 1,
+    username: 'admin',
+    password: '123456',
+    avatar: 'https://raw.githubusercontent.com/taylorchen709/markdown-images/master/vueadmin/user.png',
+    name: '张某某'
+  }
+];
+
+const Users = [];
+
+for (let i = 0; i < 86; i++) {
+  Users.push(Mock.mock({
+    id: Mock.Random.guid(),
+    name: Mock.Random.cname(),
+    addr: Mock.mock('@county(true)'),
+    'age|18-60': 1,
+    birth: Mock.Random.date(),
+    sex: Mock.Random.integer(0, 1)
+  }));
+}
+
+export { LoginUsers, Users };
